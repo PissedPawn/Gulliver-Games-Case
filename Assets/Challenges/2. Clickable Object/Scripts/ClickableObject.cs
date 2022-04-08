4e2e1395ce9dac6c531f5e79fea841911f47dc2c
Vuqar Rahim <vuqarahim@gmail.com>
2022-04-09 00:38:58 +0400

    clickable objects done

--- Assets/Challenges/2. Clickable Object/Scripts/ClickableObject.cs
+++ Assets/Challenges/2. Clickable Object/Scripts/ClickableObject.cs
@@ -1,5 +1,11 @@
 ﻿using System;
 using UnityEngine;
+using Unity.Collections;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+using TMPro;
 
 namespace Challenges._2._Clickable_Object.Scripts
 {
@@ -12,9 +18,11 @@ namespace Challenges._2._Clickable_Object.Scripts
         }
     }
     [RequireComponent(typeof(Collider))]
-    public class ClickableObject : MonoBehaviour, IClickableObject
+    public class ClickableObject : MonoBehaviour, IClickableObject, IPointerDownHandler, IPointerUpHandler
     {
 
+
+        TextMeshProUGUI clickedText;
         // Do not remove the provided 3 options, you can add more if you like
         [Flags]
         public enum InteractionMethod
@@ -24,30 +32,39 @@ namespace Challenges._2._Clickable_Object.Scripts
             TapAndHold = 8
         }
 
-        private void Update()
-        {
-
-            if (!Input.GetMouseButtonDown(0))
-                return;
-
-
-            RaycastHit hit;
-
-            Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-
-            if (Physics.Raycast(r, out hit, 100f))
-            {
-                Debug.Log(hit.transform.gameObject);
-            }
-        }
         /// <summary>
         /// Dont edit
         /// </summary>
         [SerializeField]
         private InteractionMethod allowedInteractionMethods;
 
+        public float doubleClickThreshold = 0.4f;
+        public float tapAndHoldThreshold = 0.5f;
+        public UnityEvent OnDoubleClickDetected;
+        private float lastClickTime = -1;
+        private float holdTime = -1;
+        int clicks;
+        bool loop;
+        bool hold;
 
+
+
+
+
+        void Start()
+        {
+            clickedText = GameObject.FindWithTag("Clicked").GetComponent<TextMeshProUGUI>();
+            addPhysicsRaycaster();
+        }
+
+        void addPhysicsRaycaster()
+        {
+            PhysicsRaycaster physicsRaycaster = GameObject.FindObjectOfType<PhysicsRaycaster>();
+            if (physicsRaycaster == null)
+            {
+                Camera.main.gameObject.AddComponent<PhysicsRaycaster>();
+            }
+        }
 
         /// <summary>
         /// Checks if the given interaction method is valid for this clickable object.
@@ -65,7 +82,7 @@ namespace Challenges._2._Clickable_Object.Scripts
         /// </summary>
         public void SetInteractionMethod(InteractionMethod method)
         {
-            Debug.Log(allowedInteractionMethods);
+            // Debug.Log(allowedInteractionMethods);
         }
 
 
@@ -75,6 +92,7 @@ namespace Challenges._2._Clickable_Object.Scripts
         /// <param name="callback">Function to invoke</param>
         public void RegisterToClickable(OnClickableClicked callback)
         {
+            callback(this, allowedInteractionMethods);
         }
 
         /// <summary>
@@ -83,6 +101,13 @@ namespace Challenges._2._Clickable_Object.Scripts
         /// <param name="callback">Function previously given</param>
         public void UnregisterFromClickable(OnClickableClicked callback)
         {
+            callback(this, allowedInteractionMethods);
+
+        }
+
+        public void UnregisterFromClickable(OnClickableClickedUnspecified callback)
+        {
+            callback();
         }
 
         /// <summary>
@@ -92,6 +117,7 @@ namespace Challenges._2._Clickable_Object.Scripts
         /// <exception cref="InvalidInteractionMethodException">If tapping is not allowed for this clickable</exception>
         public void RegisterToClickableTap(OnClickableClickedUnspecified onTapCallback)
         {
+            onTapCallback();
         }
 
         /// <summary>
@@ -101,7 +127,106 @@ namespace Challenges._2._Clickable_Object.Scripts
         /// <exception cref="InvalidInteractionMethodException">If double tapping is not allowed for this clickable</exception>
         public void RegisterToClickableDoubleTap(OnClickableClickedUnspecified onTapCallback)
         {
+            onTapCallback();
         }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+
+            holdTime = Time.time;
+            loop = true;
+            hold = true;
+            StartCoroutine(TapAndHoldTimer());
+            clicks++;
+
+        }
+
+
+
+        IEnumerator TapAndHoldTimer()
+        {
+            lastClickTime = Time.time;
+            while (loop)
+            {
+                if (hold)
+                {
+                    if (Time.time - holdTime > tapAndHoldThreshold)
+                    {
+                        RegisterToClickable(GetClickableObjTapAndHold);
+
+                        Debug.Log("Hold");
+                        clicks = 0;
+                        loop = false;
+                        hold = false;
+                        break;
+
+                    }
+                }
+
+
+                else if (Time.time - lastClickTime > doubleClickThreshold && clicks > 1)
+                {
+                    RegisterToClickableDoubleTap(GetClickableObjDoubleTap);
+
+
+                    clicks = 0;
+                    loop = false;
+                    Debug.Log("D tap");
+                    break;
+                }
+
+
+
+                else if (Time.time - lastClickTime > doubleClickThreshold + 0.1f)
+                {
+                    RegisterToClickableTap(GetClickableObjTap);
+
+
+                    clicks = 0;
+                    Debug.Log("Tap");
+                    loop = false;
+
+                    break;
+                }
+
+
+
+                yield return null;
+            }
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            UnregisterFromClickable(Clickable);
+            hold = false;
+        }
+
+
+        void Clickable(ClickableObject obj, InteractionMethod m)
+        {
+
+
+        }
+        void GetClickableObjDoubleTap()
+        {
+            if ((allowedInteractionMethods & (InteractionMethod.DoubleTap)) > 0)
+                clickedText.text = this.name + " : " + InteractionMethod.DoubleTap;
+        }
+
+        void GetClickableObjTap()
+        {
+
+            if ((allowedInteractionMethods & (InteractionMethod.Tap)) > 0)
+                clickedText.text = this.name + " : " + InteractionMethod.Tap;
+        }
+
+        void GetClickableObjTapAndHold(ClickableObject obj, InteractionMethod m)
+        {
+
+            if ((allowedInteractionMethods & (InteractionMethod.TapAndHold)) > 0)
+                clickedText.text = this.name + " : " + InteractionMethod.TapAndHold;
+        }
+
 
     }
 }
